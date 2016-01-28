ea312f79bdd427dec69b71a9a47144a892df1e35
Gino <ginocote@gmail.com>
2016-01-28 00:19:19 -0500

    Update server.py

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ def token():
 
   # This allows incoming connections to client (if specified)
   #client = request.values.get('client') 
-  client = 'roger' # test client fixe
+  #client = 'roger' # test client fixe
   if client != None:
     capability.allow_client_incoming(client)
   
