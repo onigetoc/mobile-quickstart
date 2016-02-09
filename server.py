d07010e0c7897a14677492131ec2d9411425a223
Gino <ginocote@gmail.com>
2016-02-09 12:21:35 -0500

    Update server.py

--- server.py
+++ server.py
@@ -90,7 +90,8 @@ if appclient != None:
   smsBody = request.values.get('Body')
   smsMedia = request.values.get('MediaUrl')
   
-  message = client.messages.create(to="+1"+smsTo, from_="+1"+smsFrom, body=smsBody)
+  message = client.messages.create(to="+1'+smsTo+'", from_="+1'+smsFrom+'", body="'+smsBody+'")
+  #message = client.messages.create(to="+1"+smsTo, from_="+1"+smsFrom, body=smsBody)
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
