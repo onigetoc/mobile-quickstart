b0ba7449fab5fabf349aff33919aeb12673dbd8a
Gino <ginocote@gmail.com>
2016-02-09 11:17:58 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, Flask, request, json
+from flask import Flask, request, json
 from twilio.util import TwilioCapability
 import twilio.twiml
 from twilio.rest import TwilioRestClient
